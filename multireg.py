13c88badff6f74f686c00277cc5986c5c79dba24
Yuzi He <yuzihe@usc.edu>
2018-08-21 15:17:36 -0700

    Add code which do Breusch-Pagan Test. Add code which test normality of residual. Add code which plot regression of single predicator.

--- multireg.py
+++ multireg.py
@@ -2,6 +2,9 @@ import sys
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import statsmodels.stats.api as sms
+from scipy import stats
+import matplotlib.pyplot as plt
 
 def do_regression(data_frame, response_var, predicators):
     Y = np.array(data_frame[response_var])
@@ -9,14 +12,51 @@ def do_regression(data_frame, response_var, predicators):
     X = sm.add_constant(X)
     linear_model = sm.OLS(Y,X)
     lr = linear_model.fit()
-    #print(X)
-    print(lr.params)
-    print(lr.bse)
-    print(abs(lr.params[1])<1.96*lr.bse[1])
-    print(lr.pvalues)
-    print(lr.ssr)
-    print(lr.rsquared)
+
+    ## make a plot
+    if(len(predicators) == 1):
+        ## plot the data points
+        plt.clf()
+        plt.scatter(X[:,1], Y[:], s=0.5, c='b')
+        ## plot the fitting line
+        x1 = np.arange(min(X[:,1]), max(X[:,1]), (max(X[:,1])-min(X[:,1]))*0.01 )
+        y1 = x1 * lr.params[1] + lr.params[0]
+        plt.plot(x1,y1,'b--')
+        plt.xlabel(predicators[0])
+        plt.ylabel(response_var)
+        plt.title(response_var+"_vs_"+predicators[0])
+        fnou = response_var+"_vs_"+predicators[0]+".pdf"
+        plt.savefig(fnou)
+        #plt.show()
+        
     
+    print("fitting parameters : ")
+    for i in range(len(lr.params)):
+        print("  %4d  %6.3g"%(i,lr.params[i]))
+    
+    print("standard error : ")
+    for i in range(len(lr.bse)):
+        print("  %4d  %6.3g"%(i,lr.bse[i]))
+        
+    print("p-value : ")
+    for i in range(len(lr.pvalues)):
+        print("  %4d  %6.3g"%(i,lr.pvalues[i]))
+    
+    #print(lr.ssr)
+    print("r-square : ", lr.rsquared)
+
+    print("** BreuschPagan test **")
+    test = sms.het_breushpagan(lr.resid, lr.model.exog)
+    name = ['Lagrange multiplier statistic', 'p-value', 
+        'f-value', 'f p-value']
+    for i in range(len(test)):
+        print("%s : %5.3f"%(name[i],test[i]))
+
+    print("** Normality of residual **")
+    k2, p = stats.normaltest(lr.resid)
+    print("p-value = ", p)
+    
+        
 if __name__ == "__main__":
     if(len(sys.argv) != 2):
         print("ERROR: Wrong number of args.")
